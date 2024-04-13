74055c00e998fc87a195cff1c8dd50761884274c
EdoDgl <ed918273645@gmail.com>
2024-04-13 08:16:29 +0300

    new pizdec

--- test.py
+++ test.py
@@ -1,6 +1,29 @@
-minus = input('Введите число \n')
+#Ввод числа пользователем
+minusInt = input('Введите число \n')
 
-if int(minus) < 0:
- print ('Otricatelnoe')
+#Проверка положительности
+if int(minusInt) < 0:
+ print ('Отрицательное \n')
 else:
- print ('Polojitelnoe')
+ print ('Положительное\n')
+
+#Проверка чётности
+if int(minusInt)%2 == 0:
+ print ('Чётное\n')
+else:
+ print ('Нечётное\n')
+
+#Ввод строки пользователем
+firstWord = input ('Введите строку\n')
+#Длина строки
+lengthFirstWord = len(firstWord)
+#Вывод длины и последнего символа строки
+print ('Количество символов: ', len(firstWord))
+print ('Последний символ: ', firstWord[lengthFirstWord - 1])
+
+secondWord = input ('Введите строку\n')
+
+if firstWord[0] == secondWord[0]:
+ print (firstWord[0], ' = ', secondWord[0])
+else:
+ print (firstWord[0], ' != ', secondWord[0])
